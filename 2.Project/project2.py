27fe2685c5fb5e971da7e21ded1ff9900eb03687
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2023-12-06 08:31:27 +0100

    Python

--- 2.Project/project2.py
+++ 2.Project/project2.py
@@ -1,10 +1,12 @@
 import random
 import sys
+import csv
 
 def main():
 
-    word_list = []
 
+    word_list = []
+    score = 0
     while True:
         level = input("Which level would you try? (type: 'l1'(easy), 'l2'(medium) or 'l3'(hard)) type 'exit program' to exit: ").lower()
         try:
@@ -19,20 +21,32 @@ def main():
         else:
             break
 
-#Shuffling and printing pair of the words to print
-    for pair_word in word_list:
-        random.shuffle(word_list)
-        eng_word = pair_word["eng"]
-        pl_word = pair_word["pl"]
-        answer = input(f"Word translation for {eng_word} is: ").lower()
-        if answer == pl_word:
-            pass
-        elif answer == "exit program":
-            exit_program()
-        else:
-            print(f"Correct word: {pl_word}")
+#Shuffling and printing pair of the words to print, scoring.
+    while word_list:
+        for pair_word in word_list:
+            random.shuffle(word_list)
+            eng_word = pair_word["eng"]
+            pl_word = pair_word["pl"]
+            answer = input(f"Word translation for {eng_word} is: ").lower()
+            if answer == pl_word:
+                word_list.remove(pair_word)
+                score += 1
+                print(f"You actual score is {score} of total {total_points} points")
+            elif answer == "exit program":
+                exit_program()
+            else:
+                print(f"Correct word: {pl_word}")
+    else:
+        print("All words translated, congratulations!")
+        exit_program()
 
-#Checking and choosing proper file with words
+#dopracowac funkcję liczenia punktów
+total_points  = 0
+for row in open("level1.csv"):
+  total_points += 1
+
+
+#Checking and choosing proper file with words and translations
 def check_lvl(level):
     if level == "l1":
         file_level = "level1.csv"
