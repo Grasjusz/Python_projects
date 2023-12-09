05583ba5531d4d97d16a6349d28032105b950bfd
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2023-12-09 00:22:51 +0000

    2. Project

--- 2.Project/project2.py
+++ 2.Project/project2.py
@@ -1,29 +1,31 @@
+"""Importing"""
 import random
 import sys
-import csv
 
 def main():
-
+    """Main program"""
 
     word_list = []
     score = 0
     while True:
-        level = input("Which level would you try? (type: 'l1'(easy), 'l2'(medium) or 'l3'(hard)) type 'exit program' to exit: ").lower()
+        language = input("Choose your language: polski, english or deutsch: ").lower()
+        level = input(check_language(language)).lower()
         try:
-            file_level = open(check_lvl(level), "r", encoding="utf-8")
-            for line in file_level:
-                line = line.lower()
-                eng, pl = line.rstrip().split(",")
-                word_list.append({"eng":eng, "pl":pl})
+            with open(check_lvl(level), "r", encoding="utf-8") as file_level:
+                for line in file_level:
+                    line = line.lower()
+                    eng, pl = line.rstrip().split(",")
+                    word_list.append({"eng":eng, "pl":pl})
         except (NameError, TypeError):
             print("You have to choose between 3 levels, type: l1, l2 or l3")
             continue
         else:
             break
 
-#Shuffling and printing pair of the words to print, scoring.
+#Shuffling and printing pair of words to print, scoring.
     while word_list:
-        for pair_word in word_list:
+        total_points = len(word_list)
+        for pair_word in word_list.copy():
             random.shuffle(word_list)
             eng_word = pair_word["eng"]
             pl_word = pair_word["pl"]
@@ -36,31 +38,38 @@ def main():
                 exit_program()
             else:
                 print(f"Correct word: {pl_word}")
-    else:
-        print("All words translated, congratulations!")
-        exit_program()
-
-#dopracowac funkcję liczenia punktów
-total_points  = 0
-for row in open("level1.csv"):
-  total_points += 1
+    return print("All words translated, congratulations!"), exit_program()
 
 
-#Checking and choosing proper file with words and translations
+def check_language(language):
+    if language == "polish":
+        language_level = "Który poziom byś wypróbował? (wpisz: 'l1' (łatwy), 'l2' (średni) lub 'l3' (trudny)) wpisz 'exit program', aby wyjść: "
+        return language_level
+    if language == "english":
+        language_level = "Which level would you try? (type: 'l1'(easy), 'l2'(medium) or 'l3'(hard)) type 'exit program' to exit: "
+        return language_level
+    if language == "german":
+        language_level = "Welches Level würdest du ausprobieren? (Typ: 'l1' (einfach), 'l2' (mittel) oder 'l3' (schwer)) Geben Sie 'exit program' ein, um zu beenden: "
+        return language_level
+    return None
+
 def check_lvl(level):
+    """Checking and choosing proper file with words and translations"""
     if level == "l1":
         file_level = "level1.csv"
         return file_level
-    elif level == "l2":
+    if level == "l2":
         file_level = "level2.csv"
         return file_level
-    elif level == "l3":
+    if level == "l3":
         file_level = "level3.csv"
         return file_level
-    elif level == "exit program":
+    if level == "exit program":
         exit_program()
+    return None
 
 def exit_program():
+    """Exit program function"""
     print("Exiting the program...")
     sys.exit(0)
 
