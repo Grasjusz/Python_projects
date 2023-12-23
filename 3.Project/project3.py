3588cdd1befc411fbb79d5d7d0c7ae6e7958a4ce
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2023-12-23 17:03:15 +0000

    Python

--- 3.Project/project3.py
+++ 3.Project/project3.py
@@ -1,23 +1,27 @@
-from pathlib import Path
-import glob
-import sys
+"""Importing"""
+import os
 
-#regex: ^[^*,'%$^&.@#!~\r\n]{1,30}\.(?:gif|png|jpe?g|bmp|webp)$
+
 
 
 def main():
     
-    path = "images/*.*"
-    list_of_files = glob.glob('images/*.*')
-    print(list_of_files)
-
-    for file in glob.glob(path):
-        print(file)
-
-#to do: slice to obtain only file name
-
-#change name all files in pattern
-
+    list_images = []
+    listed = []
+    extensions_list = ["jpg", "jpeg", "png", "bmp", "gif"]
+    path = str(input("folder with files, in program folder tree for eg: 'images/...' or 'images/': "))
+    list_of_files = os.listdir(path)
+    
+    count = 0
+    for file in list_of_files:
+        splited = file.split(".")
+        listed.append(splited)
+    for one_list in listed:
+        #os.rename
+        if len(one_list) <= 1:
+            continue
+        print(f"zdj{count:03d}.{one_list[1]}")
+        count += 1
 
 
 
