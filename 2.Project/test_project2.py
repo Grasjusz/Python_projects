2d4040b284990b20f7e4fcf771e8d92c4a1feef5
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2023-12-29 22:05:47 +0000

    python

--- 2.Project/test_project2.py
+++ 2.Project/test_project2.py
@@ -1,10 +1,25 @@
-
+import pytest
 from project2 import check_language
+from project2 import lang_selection
 
 def main():
     test_check_language()
+    test_lang_selection()
 
 
 def test_check_language():
 
     assert check_language("polski") == "Jakiego języka chcesz się uczyć? angielski lub niemiecki?: "
+    assert check_language("english") == "Which language would you like to learn? polish or german?: "
+    assert check_language("deutsch") == "Welches Sprache möchtest du lernen? polnisch oder englisch?: "
+    with pytest.raises(SystemExit) as ext:
+        check_language("exit program")
+    assert ext.type == SystemExit
+    assert ext.value.code == 0
+    
+    
+def test_lang_selection():
+    with pytest.raises(SystemExit) as ext:
+        check_language("exit program")
+    assert ext.type == SystemExit
+    assert ext.value.code == 0
