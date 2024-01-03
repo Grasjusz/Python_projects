142f4bc12b3dad2890463b3faa495409cb3d13a8
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2024-01-03 00:31:00 +0000

    python

--- 3.Project/project3.py
+++ 3.Project/project3.py
@@ -7,7 +7,8 @@ def main():
 
     list_images_src = []
     listed = []
-    path = str(input("folder with files, in program folder tree for eg: 'images/...' or 'images/': "))
+    path = str(input("""folder with files, in program folder tree for eg:
+                     'images/...' or 'images/': """))
     list_of_files = os.listdir(path)
 
     file_id = 0
@@ -15,18 +16,32 @@ def main():
         splited = file.split(".")
         listed.append(splited)
     for one_list in listed:
-        #os.rename
+    #↓ ↓ ↓ If there is a file without extension, will be omitted and removed from list ↓ ↓ ↓
         if len(one_list) <= 1:
-            copied_list = listed.copy()
-            copied_list.remove(one_list)
+            print(one_list)
+            for el_remove in one_list:
+                print(el_remove)
+                list_of_files.remove(el_remove)
             continue
         for el in list_of_files:
-            src = path + el
-            list_images_src.append(src)
-            os.rename(f"{list_images_src[file_id]}",f"{path}zdj{file_id:03d}.{one_list[1]}")
-            file_id += 1
+        #↓ ↓ ↓ If file is a directory, will be omitted and removed from list,↓ ↓ ↓
+        #↓ ↓ ↓ checking if file or directory and final renaming↓ ↓ ↓
+            is_file = os.path.isfile(f"{path}{el}")
+            print(f"{path}{el}-{is_file}")
+            if is_file is True:
+                src = path + el
+                list_images_src.append(src)
+                print(f"""{list_images_src[file_id]} changed name to:",
+                      f"{path}zdj{file_id:03d}.{one_list[1]}""")
+ #               os.rename(f"{list_images_src[file_id]}",f"{path}zdj{file_id:03d}.{one_list[1]}")
+                list_of_files.remove(el)
+                file_id += 1
+                break
+            else:
+                list_of_files.remove(el)
 
-#++++++++++++++TODO  implement not to change catalog names#++++++++++++++
+#++++++++++++++TODO  implement to chose to edit images name in all folders
+# (folder tree) or just in providen one++++++++++++++
 
 if __name__ == "__main__":
     main()
