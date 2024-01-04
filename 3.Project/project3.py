1b8d4dd95e08053187aaa0a3c4e2b79b5d3c532b
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2024-01-04 23:52:02 +0000

    python

--- 3.Project/project3.py
+++ 3.Project/project3.py
@@ -7,8 +7,9 @@ def main():
 
     list_images_src = []
     listed = []
-    path = str(input("""folder with files, in program folder tree for eg:
+    path = str(input("""path to folder with files, in program folder tree for eg:
                      'images/...' or 'images/': """))
+    naming_pattern = str(input("Pattern to name all files for eg: 'photos', 'vacation', 'kitty', etc..: "))
     list_of_files = os.listdir(path)
 
     file_id = 0
@@ -22,6 +23,12 @@ def main():
             for el_remove in one_list:
                 print(el_remove)
                 list_of_files.remove(el_remove)
+        #Rename only photos, graphic files etc.
+        if one_list[1] not in ["jpg", "jpeg", "gif", "png"]:
+            print(one_list)
+            together = f"{one_list[0]}.{one_list[1]}"
+            print(together)
+            list_of_files.remove(together)
             continue
         for el in list_of_files:
         #↓ ↓ ↓ If file is a directory, will be omitted and removed from list,↓ ↓ ↓
@@ -31,18 +38,14 @@ def main():
             if is_file is True:
                 src = path + el
                 list_images_src.append(src)
-                print(f"""{list_images_src[file_id]} changed name to:",
-                      f"{path}zdj{file_id:03d}.{one_list[1]}""")
+                print(f"""{list_images_src[file_id]} changed name to:,
+                      {path}{naming_pattern}{file_id:03d}.{one_list[1]}""")
  #               os.rename(f"{list_images_src[file_id]}",f"{path}zdj{file_id:03d}.{one_list[1]}")
                 list_of_files.remove(el)
                 file_id += 1
                 break
             else:
                 list_of_files.remove(el)
-
-#++++++++++++++TODO  implement to chose to edit images name in all folders
-# (folder tree) or just in providen one++++++++++++++
-
 if __name__ == "__main__":
     main()
     
