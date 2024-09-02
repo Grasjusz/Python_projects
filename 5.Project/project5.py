cb309940a8bfdb1cb83f846e7c6deb175f5c1386
Grasjusz <46102483+grasjusz@users.noreply.github.com>
2024-09-02 19:18:35 +0200

    new functions

--- 5.Project/project5.py
+++ 5.Project/project5.py
@@ -1,7 +1,6 @@
 """Importing"""
 import datetime
 import openpyxl
-from xlutils.copy import copy
 
 
 class Client:
@@ -33,8 +32,9 @@ class Dates:
         return accept_date, end_date
 
 def main():
-    #client = client_name_func()
-    #client_car = client_car_func()
+    client = client_name_func()
+    client_car = client_car_func()
+    print(client_car, type(client_car))
     #all_dates = dates_func()
     #car_checklist = checklist_func()
     #customer_todo = todo_func()
@@ -46,20 +46,25 @@ def main():
     template = openpyxl.load_workbook(filename = "template.xlsx")
     #Open first sheet
     f_sheet = template.active
+    #Insert data in proper columns/tables
     f_sheet["A1"] = "TEST ! ! !"
-    template.save(filename="New File.xlsx")
+    #Save document as new file with customer name and car model
+    file_name = f"{''.join(client)}{client_car['Marka']}{client_car['Model']}"
+    template.save(filename=f"{file_name}.xlsx")
 
-#to do: rest of editing template + name of file
+#to do: rest of editing template
 
 
 
 
 
 def client_name_func():
+    """Obtaining client's detail"""
     client_name = input("Imię i nazwisko klienta: ")
     return Client.client_name(client_name)
 
 def client_car_func():
+    """Obtaining vehicle's detail"""
     while True:
         car_param = {"Marka":"brak", "Model":"brak", "Rok":"brak", "Silnik":"brak", "VIN":"brak"}
         for key in car_param:
@@ -73,6 +78,7 @@ def client_car_func():
     return car_param
 
 def dates_func():
+    """Obtaining date and checking if proper format"""
     while True:
         try:
             date_format = "%d.%m.%Y"
@@ -89,6 +95,7 @@ def dates_func():
     return Dates.combined_dates(accept_date_new, end_date_new)
 
 def checklist_func():
+    """Checklist after car verification"""
     while True:
         car_checklist = {"Zawieszenie":"brak", "Oświetlenie":"brak", "Klimatyzacja":"brak",
                          "Silnik":"brak", "Koła":"brak", "Hamulce":"brak", "Nadwozie":"brak",
@@ -104,6 +111,7 @@ def checklist_func():
     return car_checklist
 
 def todo_func():
+    """"Inputing problems with car and asking if there is more"""
     todo_list = []
     while True:
         customer_report = input("Awaria, naprawa zgłoszona przez klienta: ")
@@ -118,6 +126,7 @@ def todo_func():
     return todo_list
 
 def repaired_func():
+    """Inputing reapired items and asking if more"""
     repaired_list = []
     while True:
         repaired_thing = input("Jakie naprawy zostały wykonane?: ")
@@ -132,16 +141,19 @@ def repaired_func():
     return repaired_list
 
 def repair_fast_func():
+    """inputing recommendation for short term repair"""
     repair_fast = input("Co należy zrobić jak najszybciej, w pierwszej kolejności?: ")
     recommendation = f"Pilne naprawy które należy wykonać jak najszybciej: {repair_fast}"
     return recommendation
 
 def repair_long_func():
+    """"Inputing recommendatiob for long term repair"""
     repair_in_time = input("Co należy naprawić przy następnym przeglądzie?: ")
     recommendation = f"Przed następnym przeglądem należy naprawić: {repair_in_time}"
     return recommendation
 
 def comment_func():
+    """Inputing lasts commments"""
     comments = input("Komentarz, dodatkowe informacje: ")
     last_comment = f"Komentarz, dodatkowe informacje: {comments}"
     return last_comment
